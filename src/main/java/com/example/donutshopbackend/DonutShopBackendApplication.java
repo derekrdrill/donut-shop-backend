6fbb057737c718f31d5b8cccb374cc321b44542f
Derek Drill <derekrdrill@gmail.com>
2023-04-02 13:56:54 -0400

    🎉 intial java sprint configs

---
+++ src/main/java/com/example/donutshopbackend/DonutShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.donutshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/donutshopbackend/DonutShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donutshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
